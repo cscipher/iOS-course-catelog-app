c139f9e74ee6b7eaf3a238f4f71ce18b25815a51
Harsh Soni <harsh.here17@gmail.com>
2023-01-22 17:18:54 +0530

    Basic UI structure create

--- course_catelog_app/ContentView.swift
+++ course_catelog_app/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var rupeeSymbol:String = "\u{20B9}";
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            Image("mt-1")
+                .resizable()
+                .scaledToFit()
+                .frame(height:95)
+                .padding(.trailing, 10)
+            
+            VStack(alignment: .leading, spacing: 30) {
+                Text("Course A preparation Mock test")
+                    .fontWeight(.medium)
+                    .font(.headline)
+                
+                Text("\(rupeeSymbol) 499")
+                    .fontWeight(.medium)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                
+            }
         }
-        .padding()
     }
 }
 
