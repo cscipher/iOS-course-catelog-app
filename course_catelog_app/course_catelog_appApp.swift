f8778e2ca077f19b56e9be7002c2a2e41031817a
Harsh Soni <harsh.here17@gmail.com>
2023-01-22 16:32:16 +0530

    Initial Commit

---
+++ course_catelog_app/course_catelog_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  course_catelog_appApp.swift
+//  course_catelog_app
+//
+//  Created by Harsh Soni on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct course_catelog_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
